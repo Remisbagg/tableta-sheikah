622b8934e1958410e855c7c85449717e78645693
Sergio <93742269+Remisbagg@users.noreply.github.com>
2025-02-22 13:03:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function setupChestVideos() {
                             window.location.href = 'https://www.twitch.tv/remisbagg';
                         });
                     }
-                }, 3500);
+                }, 4500);
             }
         });
 
